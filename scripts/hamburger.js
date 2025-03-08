416cf190f770bfddf3ffd52bea172eca37c2ac5e
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-03-07 22:01:17 -0500

    added products page

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,22 +1,68 @@
-// Function for hamburger menu
-document.getElementById("hamburger").addEventListener("click", function () {
-  let navBar = document.querySelector(".navbar-links");
-  let lineOne = document.querySelector(".line-1");
-  let lineTwo = document.querySelector(".line-2");
-  let lineThree = document.querySelector(".line-3");
-  let hamburger = document.getElementById("hamburger");
+const navBar = document.querySelector(".navbar-links");
+const lineOne = document.querySelector(".line-1");
+const lineTwo = document.querySelector(".line-2");
+const lineThree = document.querySelector(".line-3");
+const hamburger = document.getElementById("hamburger");
 
+hamburger.addEventListener("click", function () {
   if (navBar.style.left === "0px") {
-    navBar.style.left = "-65vw";
-    hamburger.style.transform = "rotate(0deg)";
-    lineOne.style.transform = "rotate(0deg)";
-    lineTwo.style.width = "30px";
-    lineThree.style.transform = "rotate(0deg)";
+    closeMenu();
   } else {
-    navBar.style.left = "0";
-    hamburger.style.transform = "rotate(-90deg)";
-    lineOne.style.transform = "translateY(9px) rotate(45deg)";
-    lineTwo.style.width = "0px";
-    lineThree.style.transform = "translateY(-9px)rotate(-45deg)";
+    openMenu();
   }
+});
+
+function openMenu() {
+  navBar.style.left = "0";
+  hamburger.style.transform = "rotate(-90deg)";
+  lineOne.style.transform = "translateY(9px) rotate(45deg)";
+  lineTwo.style.width = "0px";
+  lineThree.style.transform = "translateY(-9px)rotate(-45deg)";
+}
+
+function closeMenu() {
+  navBar.style.left = "-65vw";
+  hamburger.style.transform = "rotate(0deg)";
+  lineOne.style.transform = "rotate(0deg)";
+  lineTwo.style.width = "30px";
+  lineThree.style.transform = "rotate(0deg)";
+}
+
+document.body.addEventListener("click", (e) => {
+  if (
+    e.target !== navBar &&
+    e.target !== hamburger &&
+    navBar.style.left === "0px"
+  ) {
+    closeMenu();
+  }
+});
+
+// Intersection observer for animation on scroll
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        let delay = index * 200;
+        setTimeout(() => {
+          entry.target.classList.add("animate-in");
+          entry.target.classList.remove("animate-out");
+          observer.unobserve(entry.target); // Stop observing after animation
+        }, delay);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  document.querySelectorAll(".to-animate").forEach((element, index) => {
+    element.style.transitionDelay = `${index * 0.2}s`;
+    observer.observe(element);
+  });
 });
---
+++ scripts/products.js
@@ -0,0 +1,73 @@
+let cl = console.log;
+
+const gallery = document.getElementById("product-gallery-container");
+
+for (let i = 1; i <= 23; i++) {
+  gallery.innerHTML += `<div class="product-card to-animate">
+    <img src='../imgs/product-imgs/product-img(${i}).png' alt="product image">
+</div>`;
+}
+
+const productCards = document.querySelectorAll(".product-card");
+const productInfoScreen = document.getElementById("product-info-screen");
+const productInfoImg = document.getElementById("product-img-wrapper");
+const closeBtn = document.querySelector(".close-btn");
+
+if (document.body.clientWidth <= 768) {
+  productCards.forEach((card) => {
+    card.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const img = card.firstElementChild.src;
+      productInfoImg.innerHTML = `<img class="lightbox-img" src='${img}' alt="product image">`;
+      showInfoScreen();
+    });
+  });
+
+  document.body.addEventListener("click", (e) => {
+    const lightBoxImg = document.querySelector(".lightbox-img");
+    if (
+      lightBoxImg &&
+      !lightBoxImg.contains(e.target) &&
+      !e.target.closest(".product-card")
+    ) {
+      hideInfoScreen();
+    }
+  });
+
+  closeBtn.addEventListener("click", () => {
+    hideInfoScreen();
+  });
+}
+
+function showInfoScreen() {
+  document.body.style.overflow = "hidden";
+  productInfoScreen.style.display = "flex";
+  if (productInfoScreen.classList.contains("hide")) {
+    productInfoScreen.classList.remove("hide");
+  }
+  productInfoScreen.classList.add("show");
+}
+
+function hideInfoScreen() {
+  document.body.style.overflow = "auto";
+  productInfoScreen.classList.add("hide");
+  setTimeout(() => {
+    productInfoScreen.classList.remove("show");
+    productInfoScreen.style.display = "none";
+  }, 200);
+}
+
+// Sticky scroll navbar
+let lastScrollY = window.scrollY;
+const header = document.getElementById("products-header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down, hide navbar
+    header.classList.add("hidden");
+  } else {
+    // Scrolling up, show navbar
+    header.classList.remove("hidden");
+  }
+  lastScrollY = window.scrollY;
+});
