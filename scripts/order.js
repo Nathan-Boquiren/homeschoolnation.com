270357e577faf46f6d2cfadc8896a79fe4b5a208
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-03-14 13:10:23 -0400

    embed order form

--- scripts/order.js
+++ scripts/order.js
@@ -2,6 +2,44 @@ let cl = console.log;
 
 // ===== DOM Elements =====
 const header = document.getElementById("order-header");
+const orderForm = document.getElementById("order-form");
+const labels = document.querySelectorAll("label");
+const inputs = document.querySelectorAll("input");
+const phoneInput = document.getElementById("phone-input");
+
+// Order Form
+
+labels.forEach((label) => {
+  label.addEventListener("click", () => {
+    let input = label.nextElementSibling;
+    input.focus();
+  });
+});
+
+function updateFilledClass(input) {
+  let parentWrapper = input.parentElement;
+  if (input.value.trim() !== "") {
+    parentWrapper.classList.add("filled");
+  } else {
+    parentWrapper.classList.remove("filled");
+  }
+}
+
+inputs.forEach((input) => {
+  updateFilledClass(input);
+  input.addEventListener("input", () => updateFilledClass(input));
+});
+
+let telNumInput = "";
+
+phoneInput.addEventListener("input", (e) => {
+  telNumInput += e.data;
+  if (telNumInput.length === 3 || telNumInput.length === 6) {
+    phoneInput.value += "-";
+  } else if (telNumInput.length === 10 || phoneInput.value.length === 0) {
+    telNumInput = "";
+  }
+});
 
 // ===== Sticky scroll navbar =====
 let lastScrollY = window.scrollY;
