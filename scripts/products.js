96c2bc17f447cb1d4040820cfa905d0167692697
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-03-14 15:19:12 -0400

    added product data

--- scripts/products.js
+++ scripts/products.js
@@ -1,11 +1,34 @@
 let cl = console.log;
 
+// DOM Elements
+const header = document.getElementById("products-header");
 const gallery = document.getElementById("product-gallery-container");
 
-for (let i = 1; i <= 21; i++) {
-  gallery.innerHTML += `<div class="product-card to-animate">
-    <img src='../imgs/product-imgs/product-img(${i}).png' alt="product image">
-</div>`;
+// fetch product data
+
+fetch("../articles/product-data.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    populateProducts(data);
+  })
+  .catch((error) => {
+    console.error("There was a problem fetching the product data:", error);
+  });
+
+function populateProducts(data) {
+  for (let i = 0; i < data.length; i++) {
+    gallery.innerHTML += `<div class="product-card to-animate">
+      <img src='../imgs/product-imgs/product-img(${data[i].index}).png' alt="product image">
+      <h4 class="product-name">${data[i].name}</h4>
+      <h4 class="product-id">${data[i].id}</h4>
+  </div>`;
+  }
+  animateProducts();
 }
 
 const productCards = document.querySelectorAll(".product-card");
@@ -18,7 +41,6 @@ if (document.body.clientWidth <= 768) {
     card.addEventListener("click", (event) => {
       event.preventDefault();
       event.stopPropagation();
-      // cl("clicked");
       const img = card.firstElementChild.src;
       productInfoImg.innerHTML = `<img class="lightbox-img" src='${img}' alt="product image">`;
       showInfoScreen();
@@ -59,8 +81,36 @@ function hideInfoScreen() {
   }, 200);
 }
 
+// Intersection observer for animation on scroll
+function animateProducts() {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        let delay = index * 200;
+        setTimeout(() => {
+          entry.target.classList.add("animate-in");
+          entry.target.classList.remove("animate-out");
+          observer.unobserve(entry.target); // Stop observing after animation
+        }, delay);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  document.querySelectorAll(".to-animate").forEach((element, index) => {
+    element.style.transitionDelay = `${index * 0.2}s`;
+    observer.observe(element);
+  });
+}
+
 // Sticky scroll navbar
-const header = document.getElementById("products-header");
 
 let lastScrollY = 0;
 
